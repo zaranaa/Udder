b663704d91d46ba9301330f8afc7b947a7d77b74
Eryushion Techsol <eryus@Eryushions-MacBook-Pro.local>
2019-01-25 10:34:31 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Eryushion Techsol on 25/01/19.
+//  Copyright © 2019 Eryushion Techsol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
